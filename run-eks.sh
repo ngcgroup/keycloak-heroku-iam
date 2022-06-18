067545db64a89ad41b38fe1bf695c8c37bb4ca52
John Ruben <John.Ruben@bhnetwork.com>
2022-06-17 20:00:50 -0700

    Adding kube changes

---
+++ run-eks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+
+export app='/arch/bhn/iam/'
+app2=$(echo $app | sed 's/\//\\\//g')
+TEMPIFS=$IFS; 
+IFS=$'\n'; 
+for line in $(aws ssm get-parameters-by-path --path $app --query "Parameters[*].{Name:Name,Value:Value}" | jq -r '.[] |[.Name, .Value] | @tsv' | sed "s/${app2}//g" | sed "s/^\///g" |awk -F '\t' '{print $1"="$2}'); do
+	if [[ "$line" =~ ^([a-zA-Z0-9_]*)=(.*)$ ]]; then 
+		export ${BASH_REMATCH[1]}=${BASH_REMATCH[2]}; 
+	fi; 
+done; 
+IFS=$TEMPIFS;
+
+kubectl apply -f keycloak-service.yaml,keycloak-deployment.yaml
