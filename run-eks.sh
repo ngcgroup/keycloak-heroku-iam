dd3949d4e18fbc75a5b6492edc57ee088b2d95fd
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2022-06-18 14:51:31 -0700

    test

--- run-eks.sh
+++ run-eks.sh
@@ -2,12 +2,35 @@
 set -e
 set -x
 
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--profile)
+        profile=$2
+        shift # past argument
+        shift # past value
+        ;;       
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+if [ "$profile" == "" ]; then 
+	profile="arch"
+fi
+
 export app='/arch/bhn/iam/'
 app2=$(echo $app | sed 's/\//\\\//g')
 TEMPIFS=$IFS; 
 IFS=$'\n'; 
 envargs=""
-profile="arch"
+
 for line in $(aws ssm get-parameters-by-path --profile ${profile} --path $app --query "Parameters[*].{Name:Name,Value:Value}" | jq -r '.[] |[.Name, .Value] | @tsv' | sed "s/${app2}//g" | sed "s/^\///g" |awk -F '\t' '{print $1"="$2}'); do
 	if [[ "$line" =~ ^([a-zA-Z0-9_]*)=(.*)$ ]]; then 
 		export ${BASH_REMATCH[1]}=${BASH_REMATCH[2]}; 
