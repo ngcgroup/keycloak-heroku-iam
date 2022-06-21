5b347129ea0fefe5eb3f47897c45307cf6995fcb
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2022-06-20 19:24:03 -0700

    keystore update

--- generate-keystore.sh
+++ generate-keystore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 keytool -genkey -alias server -keyalg RSA -keysize 2048 -validity 3650 -keystore application.keystore -dname "CN=localhost,OU=Technology,O=BHN,L=Pleasanton,S=CA,C=US" -storepass password -keypass password -noprompt -ext SAN=dns:iam.bhn.technology
-kubectl create secret generic keycloak-keystore -n iam --from-file application.keystore=application.keystore
+keytool -export -alias server -file keycloak.crt -keystore application.keystore -storepass password -noprompt
+kubectl create secret generic keycloak-keystore -n iam --from-file application.keystore=application.keystore --from-file=keycloak.crt=keycloak.crt
 kubectl delete pod/keycloak-0 -n iam
 kubectl label secret keycloak-keystore -n iam app=kubed
-
 
 #### junk ### 
 # $ $(cat <<EOF | kubectl exec  -n iam -it keycloak-0 -- bash
